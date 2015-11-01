ce036736885417669c11843258daf617dcb8b96f
shijiezhao <shijie0213@gmail.com>
2015-11-01 02:01:52 -0500

    GU

--- svm_test.py
+++ svm_test.py
@@ -12,7 +12,7 @@ Some functions
 X = np.array([[1,2],[2,2],[0,0],[-2,3]]).copy()
 Y = np.array([-1.,-1.,1.,1.]).copy()
 
-trainer = svm.SVMTrainer(svm.Kernel.linear(),c=0.2)
+trainer = svm.SVMTrainer(svm.Kernel.gaussian(5),c=200)
 predictor = trainer.train(X,Y)
 plotFigure.plot(predictor, X,Y, grid_size=100, filename='TEST.pdf')
 
